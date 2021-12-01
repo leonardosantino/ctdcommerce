a9907395dfb8b96a6f4bdafefadd10a879ee5787
unknown <79323700+leonardosantino@users.noreply.github.com>
2021-12-01 14:58:23 -0300

    component product

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,24 @@ import "../assets/styles/home.scss"
 import Header from "../components/Header"
 import Carousel from "../components/Carousel";
 import Card from "../components/Card";
+import Product from "../components/Product";
 
 function Home() {
     return (
       <>
         <Header/>
+
         <Carousel/>
+
         <div className="d-flex justify-content-between px-5">
         <Card/> <Card/> <Card/> <Card/> <Card/>
         </div>
-        
+
+        <div className="p-5">
+        <Product />
+        </div>
+
       </>
     );
   }
-  
   export default Home;
