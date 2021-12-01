cc6801a92021573b8a8dae6c3393d62468585059
unknown <79323700+leonardosantino@users.noreply.github.com>
2021-11-30 23:28:55 -0300

    carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,17 @@
 import imgCarousel1 from "../assets/imgs/imgCarousel1.png"
+import imgCarousel2 from "../assets/imgs/imgCarousel2.png"
 import "../assets/styles/carousel.scss"
 
 function Carousel() {
   return (
-    <> <div className="carouselsection p-5">
+    <> <div className="carouselsection  ps-5">
       <div id="carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
+          <div class="carousel-item active" data-bs-interval="10000">
             <img src={imgCarousel1} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={imgCarousel2} class="d-block w-100" alt="..." />
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
