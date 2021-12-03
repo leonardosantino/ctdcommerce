f837afaf8b046260e428766a66fb957657afb378
unknown <79323700+leonardosantino@users.noreply.github.com>
2021-12-03 20:45:44 -0300

    fix carousel cards

--- src/components/CarouselCard.jsx
+++ src/components/CarouselCard.jsx
@@ -1,7 +1,12 @@
 import "../assets/styles/carouselcard.scss"
 import Card from "./Card";
 
-const imgCard1 = "images/games/imgGame1.png"
+const imgCard7 = "images/games/imgGame7.png"
+const imgCard8 = "images/games/imgGame8.png"
+const imgCard9 = "images/games/imgGame9.png"
+const imgCard10 = "images/games/imgGame10.png"
+const imgCard11 = "images/games/imgGame11.png"
+const imgCard12 = "images/games/imgGame12.png"
 
 function CarouselCard() {
   return (
@@ -15,13 +20,17 @@ function CarouselCard() {
         <div id="carouselCard" className="carousel slide" data-bs-ride="carouselCard">
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <div className="d-flex justify-content-center">
-                <Card img={imgCard1}/>
+              <div className="d-flex justify-content-evenly">
+                <Card img={imgCard7} title={"Watch Dogs Legion"} description={"Ubisoft"} console={"PS4"} valor={89.90}/>
+                <Card img={imgCard8} title={"Cloudpunk"} description={"Maple Whispering / Ion Lands"} console={"XBOX"} valor={109.90}/>
+                <Card img={imgCard9} title={"Crysis Remastered"} description={"Crytek"} console={"PS4"} valor={79.90}/>
               </div>
             </div>
             <div className="carousel-item">
-              <div className="d-flex justify-content-center">
-                <Card img={imgCard1} />
+              <div className="d-flex justify-content-evenly">
+              <Card img={imgCard10} title={"Far Cry 6"} description={"Ubisoft"} console={"XBOX"} valor={129.90}/>
+              <Card img={imgCard11} title={"This Is The Zodiac Spea..."} description={"Klabater / Punch Punk Games"} console={"PS4"} valor={109.90}/>
+              <Card img={imgCard12} title={"NBA 2K21"} description={"2K Sports / Visual Concepts"} console={"XBOX"} valor={129.90}/>
               </div>
             </div>
           </div>
