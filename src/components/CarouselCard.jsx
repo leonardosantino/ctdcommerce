1a57360ff98eecb5807f216b3371bc90fc0c0f48
unknown <79323700+leonardosantino@users.noreply.github.com>
2021-12-05 15:51:28 -0300

    merge CarouselCard.jsx

--- src/components/CarouselCard.jsx
+++ src/components/CarouselCard.jsx
@@ -19,32 +19,12 @@ function CarouselCard() {
         <hr />
       </div>
 
-<<<<<<< HEAD
-      <div className="carouselsection">
-        <div id="carouselCard" className="carousel slide" data-bs-ride="carouselCard">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <div className="d-flex justify-content-evenly">
-                <Card img={imgCard7} title={"Watch Dogs Legion"} description={"Ubisoft"} console={"PS4"} valor={89.90} id={7}/>
-                <Card img={imgCard8} title={"Cloudpunk"} description={"Maple Whispering / Ion Lands"} console={"XBOX"} valor={109.90} id={8}/>
-                <Card img={imgCard9} title={"Crysis Remastered"} description={"Crytek"} console={"PS4"} valor={79.90} id={9}/>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="d-flex justify-content-evenly">
-              <Card img={imgCard10} title={"Far Cry 6"} description={"Ubisoft"} console={"XBOX"} valor={129.90} id={10}/>
-              <Card img={imgCard11} title={"This Is The Zodiac Spea..."} description={"Klabater / Punch Punk Games"} console={"PS4"} valor={109.90} id={11}/>
-              <Card img={imgCard12} title={"NBA 2K21"} description={"2K Sports / Visual Concepts"} console={"XBOX"} valor={129.90} id={12}/>
-              </div>
-            </div>
-=======
       <Carousel>
         <Carousel.Item interval={1500}>
           <div className="d-flex justify-content-evenly">
             <Card img={imgCard7} title={"Watch Dogs Legion"} description={"Ubisoft"} console={"PS4"} valor={89.90} id={1} />
             <Card img={imgCard8} title={"Cloudpunk"} description={"Maple Whispering / Ion Lands"} console={"XBOX"} valor={109.90} id={2} />
             <Card img={imgCard9} title={"Crysis Remastered"} description={"Crytek"} console={"PS4"} valor={79.90} id={3} />
->>>>>>> 95c0203858bd33fe5b00b59cedce174fa9648972
           </div>
         </Carousel.Item>
         <Carousel.Item>
