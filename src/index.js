e31464d5c73594711890bb3e3bc9b40420c86950
unknown <79323700+leonardosantino@users.noreply.github.com>
2021-12-06 11:16:42 -0300

    remove bootstrap links from index html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/index.scss'
 import App from './container/App';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
