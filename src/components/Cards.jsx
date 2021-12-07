dc94625f6f56232350febb05e8c8e96f990ac744
rafa meira <meirarafaela@hotmail.com>
2021-12-06 21:26:04 -0300

    alteracao react-bootstrap cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,11 @@
 import "../assets/styles/card.scss"
-import { NavLink } from "react-router-dom";
-import { useCount } from "../context/Count";
-import { useEffect } from "react";
+import {NavLink} from "react-router-dom";
+import {useCount} from "../context/Count";
+import {useEffect} from "react";
+import {Button, Card} from "react-bootstrap";
 
-function Card({ img, title, description, console, valor, id }) {
-    const { count, setCount, product, setProduct } = useCount();
+function Cards({img, title, description, console, valor, id}) {
+    const {count, setCount, product, setProduct} = useCount();
 
     useEffect(() => {
 
@@ -17,38 +18,54 @@ function Card({ img, title, description, console, valor, id }) {
 
         const findproduct = product.find((element, index) => {
             productindex = index
-            return    element.idproduct === id      
+            return element.idproduct === id
         });
 
-        if(findproduct === undefined){
+        if (findproduct === undefined) {
 
             let tempProduct = [...product];
-            tempProduct.push({idproduct: id, quantity: 1, imgproduct: img, titleproduct: title, descriptionproduct: description ,valorproduct: valor})
+            tempProduct.push({
+                idproduct: id,
+                quantity: 1,
+                imgproduct: img,
+                titleproduct: title,
+                descriptionproduct: description,
+                valorproduct: valor
+            })
             setProduct(tempProduct)
-            setCount(count +1)
+            setCount(count + 1)
 
-        }else{
-
+        } else {
             let tempProduct = [...product];
-            let tempElement = { ...tempProduct[productindex] }
+            let tempElement = {...tempProduct[productindex]}
             tempElement.quantity += 1
             tempProduct[productindex] = tempElement
             setProduct(tempProduct)
-
         }
     }
+
     return (
         <>
-            <div className="card">
-                <img src={img} className="card-img-top" alt="Card img" />
-                <div className="card-body">
-                    <div className="card-title"><NavLink className="card-title text-decoration-none" to={`/games/${id}`} >{title}</NavLink>  </div>
-                    <div className="card-text">{description}</div>
+            <Card className="card">
+                <Card.Img variant="top" src={img} alt={title}/>
+                <Card.Body className="card-body">
+                    <Card.Title>
+                        <NavLink className="card-title text-decoration-none" to={`/games/${id}`}>{title}</NavLink>
+                    </Card.Title>
+                    <Card.Text className="card-text">{description}</Card.Text>
                     <div className="card-cat my-2"><span className="consol">{console}</span></div>
-                    <div className="card-btn my-2"><button onClick={addToCart} className="btn btn-primary"><i className="bi bi-bag"></i>&nbsp; R$ {valor.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</button></div>
-                </div>
-            </div>
+                    <div className="card-btn my-2">
+                        <Button onClick={addToCart} variant="primary">
+                            <i className="bi bi-bag"></i>&nbsp; R$ {valor.toLocaleString(undefined, {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2
+                        })}
+                        </Button>
+                    </div>
+                </Card.Body>
+            </Card>
         </>
-    );
+    )
 }
-export default Card;
+
+export default Cards;
